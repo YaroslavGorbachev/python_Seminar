7ffe7ffb872c530e90c59ec6f59022a60e713940
Yaroslav <104135869+YaroslavGorbachev@users.noreply.github.com>
2022-07-08 00:07:32 +0700

    Seminar_1

---
+++ seminar_1.1.py
@@ -0,0 +1,44 @@
+# num_1 = int(input('введите число один '))
+# num_2 = int(input('введите число два '))
+# if num_1**2 == num_2:
+#     print('Квадрат первого числа - это второе число')
+# elif num_2**2 == num_1:
+#     print('Квадрат второго числа - это первое число')
+
+# a = list() #[] <== тотже лист
+# for i in range(5): #range создаст список включительно до числа которое стоит число
+# #range (1,11,2) с настройкай шага
+#     a.append(int(input('-->'))) #append 
+# maxx = a[0]
+# for i in range(1,5):
+#     if a[i] > maxx:
+#         maxx = a[i]
+# print(maxx)
+
+# n = int(input('Введите N: '))
+# i = -n
+# while i <= n:
+#     print(i) #end=перенос в строку print(i,end='|')
+#     i+=1
+
+
+# #% - Остаток от деления
+# #// - целое число
+# x = int(input('введите число-'))
+# if((x % 5 == 0 and x % 10 == 0) or (x % 15 == 0 and not x % 30 == 0)): #!=не ровно 
+#     print('TRUE')
+# else:
+#     print('FALSE')
+
+# # a = float(input('-->'))
+# # b = int(a * 10)
+# # print(b % 10)
+
+# x = input('x = ')
+# for i in range(len(x)):
+#     if x[i] == ',':
+#         print(x[i + 1])
+#         break
+
+
+
